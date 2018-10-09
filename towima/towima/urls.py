7d822582363584e966e73231306c88cfb2dc4789
maximeantoine1997 <maximeantoine1997@gmail.com>
2018-10-09 14:19:05 +0200

    Basic Signup page

--- towima/towima/urls.py
+++ towima/towima/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from towima import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url( r'^signup/$', core_views.signup, name='signup'),
 ]
