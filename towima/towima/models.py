ecaa6180b5feb6371514f9f3cf491397527ed9a3
Willem Röpke <willem.ropke@vub.be>
2018-10-23 11:49:15 +0200

    deleted the snippets folder and added its functionality elsewhere

--- towima/towima/models.py
+++ towima/towima/models.py
@@ -6,4 +6,41 @@ class Profile(models.Model):
     phone_number = models.CharField(max_length=12)
     date_of_birth = models.DateField()
     address = models.CharField(max_length = 50)
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('towima:product_list_by_category', args=[self.slug])
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True) #pip install pillow
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),) 
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+       return reverse('towmima:product_detail', args[self.id, self.slug])
     
