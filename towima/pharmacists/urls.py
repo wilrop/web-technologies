aacea46b156798669ac42ea433f59d9c67130153
Willem Röpke <willemropke@hotmail.com>
2018-10-31 00:06:20 +0100

    A user can now click on a selected pharmacist and go to his or her public profile

--- towima/pharmacists/urls.py
+++ towima/pharmacists/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.conf.urls import url
 from pharmacists import views
 
 urlpatterns = [
-    path('list/', views.pharmacists_list, name='list')
+    path('list/', views.pharmacists_list, name='list'),
+    #path('profile/', views.pharmacists_profile, name='profile'),
+    url(r'^profile/(?P<pk>\d+)/$', views.pharmacists_profile, name='profile')
 ]
