8d41f414ff0709483ee5deed2514959314059e03
samohtforce <thomas.lieven.vaeyens@vub.be>
2018-10-31 11:54:48 +0100

    Made a seperate application for the products

--- towima/towima/settings.py
+++ towima/towima/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',     # The account app
     'towima',                           # Our own app
+    'products.apps.ProductsConfig', #products App
 ]
 
 MIDDLEWARE = [
