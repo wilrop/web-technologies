6f6e9e83893cd1b9f3f5b955cb16c75fd24c54b7
samohtforce <thomas.lieven.vaeyens@vub.be>
2018-11-10 09:53:43 +0100

    I modified the admin page for the Pharmacy

--- towima/pharmacists/admin.py
+++ towima/pharmacists/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from pharmacists.models import Pharmacy
 
-# Register your models here.
+class PharmacyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'phone_number', 'address']
+    list_filter = ['name']
+    list_editable = ['phone_number', 'address']
+admin.site.register(Pharmacy, PharmacyAdmin)
