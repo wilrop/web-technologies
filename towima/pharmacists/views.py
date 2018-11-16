0446a9c64b0c32baebdc1eb2b43d9046f17c0053
maximeantoine1997 <maximeantoine1997@gmail.com>
2018-11-16 17:23:33 +0100

    changed the GUI

--- towima/pharmacists/views.py
+++ towima/pharmacists/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from accounts.models import Profile
 from pharmacists.forms import PharmacyForm
 from django.contrib.auth.models import User
+from mapbox import Directions
 
 
 def pharmacists_list(request):
@@ -15,6 +16,27 @@ def pharmacists_profile(request, pk):
 
 def find_pharma(request):
     mapbox_access_token = 'pk.my_mapbox_access_token'
+    print("hello")
+    service = Directions()
+    origin = {
+        'type': 'Feature',
+        'properties': {'name': 'Portland, OR'},
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [-122.7282, 45.5801]}}
+    destination = {
+        'type': 'Feature',
+        'properties': {'name': 'Bend, OR'},
+        'geometry': {
+            'type': 'Point',
+            'coordinates': [-121.3153, 44.0582]}}
+    if request.method == 'GET': 
+        print("hello")
+        response = service.directions([origin, destination], 'mapbox/driving')
+        
+    return render(request, 'pharmacists/find_pharma.html', {'mapbox_access_token': mapbox_access_token})
+
+def create_pharmacy(request):
     if request.method == 'POST':                # When we POST the form.
         form = PharmacyForm(request.POST)
         if form.is_valid():                     # Check if the form is valid.
@@ -25,7 +47,7 @@ def find_pharma(request):
             return redirect ('home') 
     else:                                       
         form = PharmacyForm() 
-    return render(request, 'pharmacists/find_pharma.html', {'mapbox_access_token': mapbox_access_token, 'form': form})
+    return render(request, 'pharmacists/create_pharmacy.html', {'form': form})
 
 
     
