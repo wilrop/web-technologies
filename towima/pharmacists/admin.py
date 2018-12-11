a61b74f1a22ac74ced6399ca4db75a01de6be640
Willem Röpke <willemropke@hotmail.com>
2018-12-11 18:05:08 +0100

    Merged with Thomas

--- towima/pharmacists/admin.py
+++ towima/pharmacists/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from pharmacists.models import Pharmacy, Comments, Rating
 
 class PharmacyAdmin(admin.ModelAdmin):
@@ -7,9 +6,6 @@ class PharmacyAdmin(admin.ModelAdmin):
     list_filter = ['name']
     list_editable = ['phone_number', 'address']
 admin.site.register(Pharmacy, PharmacyAdmin)
-=======
-from pharmacists.models import Comments
->>>>>>> 9b582bd3a4eebe85db60551cf86a0ba3c43e532c
 
 admin.site.register(Comments)
 admin.site.register(Rating)
