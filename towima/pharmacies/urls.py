66a032dd1582ee5b17f562719f21fa6c0935c993
Thomas Vaeyens <thomasvaeyens@MacBook-Pro-van-Thomas.local>
2018-12-11 15:50:32 +0100

    I added a profile page for the pharmacy

--- towima/pharmacies/urls.py
+++ towima/pharmacies/urls.py
@@ -5,5 +5,5 @@ from pharmacies import views
 app_name = 'pharmacies'
 urlpatterns = [
     path('create_pharma/', views.create_pharma, name='create_pharma'),
-    url(r'^(?P<pharmacy_id>\d+)/(?P<slug>[-\w]+)/$', views.pharmacy_detail, name='pharmacy_detail'),
+    url(r'^(?P<pharmacy_id>\d+)/(?P<slug>[-\w]+)/$', views.pharmacy_detail, name='pharmacy_detail')
 ]
--- towima/pharmacies/views.py
+++ towima/pharmacies/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect, get_list_or_404, get_object_or_404
-from pharmacies.models import Pharmacy
+from pharmacies.models import Pharmacy, Employee
 from pharmacies.forms import PharmacyForm
 
 def pharmacy_detail(request, pharmacy_id, slug):
-    pharmacy = get_object_or_404(Pharmacy, id=pharmacy_id, slug=slug, available=True)
-    args = {'pharmacy': pharmacy,}
-    return render(request, 'pharmacy/detail.html', args)
+    pharmacy = get_object_or_404(Pharmacy, id=pharmacy_id, slug=slug)
+    entries = Employee.objects.filter(pharmacy=pharmacy.id)
+    args = {'pharmacy': pharmacy, 'entries': entries}
+    return render(request, 'pharmacies/pharmacy_profile.html', args)
 
 def create_pharma(request):
     if request.method == 'POST':                # When we POST the form.
