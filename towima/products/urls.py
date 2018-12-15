977d9c1817856805edc27ed3e0acd731891adca8
samohtforce <thomas.lieven.vaeyens@vub.be>
2018-12-15 12:12:05 +0100

    I added a form for adding a product

--- towima/products/urls.py
+++ towima/products/urls.py
@@ -5,5 +5,6 @@ from products import views
 app_name = 'products'
 urlpatterns = [
     path('list/', views.products_list, name='list'),
+    path('add/', views.add_product, name='add_product'),
     url(r'^(?P<product_id>\d+)/(?P<slug>[-\w]+)/$', views.product_detail, name='product_detail')
 ]
