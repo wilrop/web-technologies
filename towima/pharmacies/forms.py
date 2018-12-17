0c83fadffc17e37e269ff55f75c0b6623c9efe71
samohtforce <thomas.lieven.vaeyens@vub.be>
2018-12-18 00:02:50 +0100

    I added some buttons and order pages

--- towima/pharmacies/forms.py
+++ towima/pharmacies/forms.py
@@ -1,6 +1,8 @@
 from django import forms
-from pharmacies.models import Pharmacy, Comments
+from pharmacies.models import Pharmacy, Comments, Stock
+from products.models import Product
 from accounts.models import Item
+from django.contrib.auth.models import User
 
 class PharmacyForm(forms.ModelForm):
     name = forms.CharField(max_length = 70)
@@ -12,6 +14,15 @@ class PharmacyForm(forms.ModelForm):
         model = Pharmacy
         fields = ('name','address','phone_number')
         widgets = {'slug': forms.HiddenInput()}
+
+    def save(self, user, commit = True):
+        user_id = User.objects.get(username=user)
+        name = self.cleaned_data['name']
+        phone_number = self.cleaned_data['phone_number']
+        address = self.cleaned_data['address']
+        if commit:         
+            pharmacy = Pharmacy(owner=user_id, name=name, address=address, phone_number=phone_number)
+            pharmacy.save()  
 
 class CommentForm(forms.ModelForm):
     class Meta:
@@ -36,3 +47,25 @@ class AddToCartForm(forms.ModelForm):
         fields = (              # The order of the fields.
             'quantity',
         )
+
+class AddStockForm(forms.ModelForm):
+    product = forms.ModelChoiceField(queryset=Product.objects.all())
+    stock = forms.IntegerField()
+    price = forms.DecimalField()
+
+    class Meta:
+        model = Stock
+        fields = (              # The order of the fields.
+            'product',
+            'stock',
+            'price',
+        )
+    def save(self, pharmacy, commit = True):
+        print("binnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnen")
+        product = self.cleaned_data['product']
+        stock = self.cleaned_data['stock']
+        price = self.cleaned_data['price']
+        if commit:
+            if not Stock.objects.filter(pharmacy=pharmacy, product=product):
+                stock = Stock(product=product, pharmacy=pharmacy, product_stock=stock, product_price=price)
+                stock.save()
