528ca82cc5bef5de115f8ea8f2eee232aab52eab
Thomas Vaeyens <thomasvaeyens@MacBook-Pro-van-Thomas.local>
2018-12-18 11:51:45 +0100

    I added some modifications to the rest framework

--- towima/api/views.py
+++ towima/api/views.py
@@ -8,23 +8,29 @@ from api.serializers import ProductSerializer, CategorySerializer, CommentsSeria
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    http_method_names = ['get']
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    http_method_names = ['get']
 
 class CommentsViewSet(viewsets.ModelViewSet):
     queryset = Comments.objects.all()
     serializer_class = CommentsSerializer
+    http_method_names = ['get']
 
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
+    http_method_names = ['get']
 
 class PharmacyViewSet(viewsets.ModelViewSet):
     queryset = Pharmacy.objects.all()
     serializer_class = PharmacySerializer
+    http_method_names = ['get']
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    http_method_names = ['get']
