60ea31f763d2fbadb74d6144a5249925392a1202
Willem Röpke <willem.ropke@vub.be>
2019-02-09 16:47:33 +0100

    still trying fml

--- towima/towima/settings.py
+++ towima/towima/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'accounts',         # The account app
+    'accounts',         # The account app
     'towima',           # Our own app
     'products',         #products App
     'pharmacies',       #Pharmacy App
