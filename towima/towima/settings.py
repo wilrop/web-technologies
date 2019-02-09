4ea20ebe16e6cb6e70e003488eee6986e3d68d56
Willem Röpke <willem.ropke@vub.be>
2019-02-09 14:37:40 +0100

    Changed whitenoise path

--- towima/towima/settings.py
+++ towima/towima/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
