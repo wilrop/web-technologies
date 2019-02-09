55fb60ae201588bb0f46136a4861ce775582a969
Willem Röpke <willem.ropke@vub.be>
2019-02-10 00:34:27 +0100

    changed it back lol

--- towima/towima/settings.py
+++ towima/towima/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts.apps.AccountsConfig',         # The account app
+    'accounts',         # The account app
     'towima',           # Our own app
     'products',         #products App
     'pharmacies',       #Pharmacy App
